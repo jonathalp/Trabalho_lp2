557ec9486b838d80dc44eec10b858edaa17b37ec
Diego Rossi <diegorossi8@gmail.com>
2017-03-31 10:27:42 -0500

    Dia 30

--- Trabalho LP2/src/Cadastro1.java
+++ Trabalho LP2/src/Cadastro1.java
@@ -1,19 +1,16 @@
-import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
+import java.awt.Font;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import java.awt.Font;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 public class Cadastro1 extends JFrame implements ActionListener {
 
@@ -36,9 +33,8 @@ public class Cadastro1 extends JFrame implements ActionListener {
 		btnRetornarLogin.setContentAreaFilled(false);
 		btnRetornarLogin.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 		btnRetornarLogin.setBounds(190, 454, 166, 63);
-		
+
 		contentPane.add(btnRetornarLogin);
-		
 
 		btnPessoaJ.setOpaque(false);
 		btnPessoaJ.setFont(new Font("Tahoma", Font.BOLD, 16));
@@ -46,20 +42,19 @@ public class Cadastro1 extends JFrame implements ActionListener {
 		btnPessoaJ.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 		btnPessoaJ.setBounds(299, 192, 223, 63);
 		contentPane.add(btnPessoaJ);
-		
-		
+
 		btnPessoaF.setOpaque(false);
 		btnPessoaF.setContentAreaFilled(false);
 		btnPessoaF.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 		btnPessoaF.setFont(new Font("Tahoma", Font.BOLD, 16));
 		btnPessoaF.setBounds(10, 192, 223, 63);
 		contentPane.add(btnPessoaF);
-		
+
 		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setIcon(new ImageIcon("C:\\Imagens\\Fundo cad ini.png"));
 		lblNewLabel.setBounds(0, 0, 539, 517);
 		contentPane.add(lblNewLabel);
-		
+
 		btnPessoaF.addActionListener(this);
 		btnPessoaJ.addActionListener(this);
 		btnRetornarLogin.addActionListener(this);
@@ -67,17 +62,21 @@ public class Cadastro1 extends JFrame implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() == btnRetornarLogin){
+		if (e.getSource() == btnRetornarLogin) {
 			Login login = new Login();
 			login.setVisible(true);
 			this.dispose();
 		}
-		if(e.getSource() == btnPessoaF){
+		if (e.getSource() == btnPessoaF) {
 			CadastroFisico cadastrofisico = new CadastroFisico();
 			cadastrofisico.setVisible(true);
 			this.dispose();
 		}
-		
-		
+		if (e.getSource() == btnPessoaJ) {
+			CadastroJuridico cadastrijuridico = new CadastroJuridico();
+			cadastrijuridico.setVisible(true);
+			this.dispose();
+		}
+
 	}
 }
---
+++ Trabalho LP2/src/PrincipalPJ.java
@@ -0,0 +1,67 @@
+import java.awt.Component;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+import javax.swing.border.EmptyBorder;
+
+public class PrincipalPJ extends JFrame implements ActionListener {
+
+	JPanel contentPane;
+	JMenuItem mntmSair = new JMenuItem("Sair");
+
+	public PrincipalPJ() {
+		setTitle("Cerve...j\u00E1! Beba com modera\u00E7\u00E3o.");
+		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Imagens\\Brahma-logo.jpg"));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 567, 574);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBounds(0, 0, 550, 21);
+		contentPane.add(menuBar);
+
+		JMenu mnCadastro = new JMenu("Cadastro");
+		menuBar.add(mnCadastro);
+
+		JMenuItem mntmEditar = new JMenuItem("Editar");
+		mnCadastro.add(mntmEditar);
+
+		JMenu mnConsulta = new JMenu("Consulta");
+		menuBar.add(mnConsulta);
+
+		JMenu mnSair = new JMenu("Sair");
+		menuBar.add(mnSair);
+
+		mnSair.add(mntmSair);
+
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setBounds(0, 0, 550, 532);
+		lblNewLabel.setIcon(new ImageIcon("C:\\Imagens\\Brahma-logo.jpg"));
+		contentPane.add(lblNewLabel);
+
+		mntmSair.addActionListener(this);
+	}
+
+	private static void addPopup(Component component, final JPopupMenu popup) {
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == mntmSair) {
+			this.dispose();
+		}
+
+	}
+}
